5e142bfaef80341b8dfe285143ca1594d8d1e4ab
rodrigo34892 <136374249+rodrigo34892@users.noreply.github.com>
2025-05-22 20:29:36 -0300

    testes cad/log

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {ClienteScreen} from '../views/ClienteScreen';
-import {ServicoScreen} from '../views/ServicoScreen';
-import {BarbeiroScreen} from '../components/LoginBarbeiro';
-import {ProdutoScreen} from '../views/ProdutoScreen';
-import {AgendamentoScreen} from '../views/AgendamentoScreen';
-import {DashboardScreen} from '../views/DashboardScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ClienteScreen } from '../views/ClienteScreen';
+import { ServicoScreen } from '../views/ServicoScreen';
+import { BarbeiroScreen } from '../components/LoginBarbeiro';
+import { ProdutoScreen } from '../views/ProdutoScreen';
+import { AgendamentoScreen } from '../views/AgendamentoScreen';
+import { DashboardScreen } from '../views/DashboardScreen';
 import Cadastro from '../Cadastro/Cadastro';
 import TelaLogin from '../Login/TelaLogin';
 
@@ -41,53 +41,53 @@ export const AppNavigator = () => {
         <Stack.Screen
           name="Dashboard"
           component={DashboardScreen}
-          options={{title: 'Dashboard'}}
+          options={{ title: 'Dashboard' }}
         />
         <Stack.Screen
           name="Clientes"
           component={ClienteScreen}
-          options={{title: 'Clientes'}}
+          options={{ title: 'Clientes' }}
         />
         <Stack.Screen
           name="Servicos"
           component={ServicoScreen}
-          options={{title: 'Serviços'}}
+          options={{ title: 'Serviços' }}
         />
         <Stack.Screen
           name="Barbeiros"
           component={BarbeiroScreen}
-          options={{title: 'Barbeiros'}}
+          options={{ title: 'Barbeiros' }}
         />
         <Stack.Screen
           name="Produtos"
           component={ProdutoScreen}
-          options={{title: 'Produtos'}}
+          options={{ title: 'Produtos' }}
         />
         <Stack.Screen
           name="Agendamento"
           component={AgendamentoScreen}
-          options={{title: 'Agendamentos'}}
+          options={{ title: 'Agendamentos' }}
         />
-        
+
         <Stack.Screen
           name="Cadastro"
           component={Cadastro}
-          options={{title: 'Cadastro'}}
+          options={{ title: 'Cadastro' }}
         />
- 
-        
+
+
         <Stack.Screen
           name="TelaLogin"
           component={TelaLogin}
-          options={{title: 'TelaLogin'}}
+          options={{ title: 'TelaLogin' }}
         />
 
- 
-        
+
+
         <Stack.Screen
           name="TelaTestes"
           component={TelaLogin}
-          options={{title: 'Testes'}}
+          options={{ title: 'Testes' }}
         />
 
 
